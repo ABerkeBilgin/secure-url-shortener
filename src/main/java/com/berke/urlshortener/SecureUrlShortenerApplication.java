5e00e0dac4c02a908e638a6e5f2275be2b4a43e3
Ahmet Berke Bilgin <ahmetberkeb8@gmail.com>
2026-01-18 02:54:47 +0300

    chore: initial commit

---
+++ src/main/java/com/berke/urlshortener/SecureUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.berke.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berke/urlshortener/SecureUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berke.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
