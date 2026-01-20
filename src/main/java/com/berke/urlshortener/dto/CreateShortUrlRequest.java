4ba32bf506726cf6dc829c829d43d4e44ce3e486
Ahmet Berke Bilgin <ahmetberkeb8@gmail.com>
2026-01-20 21:24:54 +0300

    feat: add DTO classes for API contract

---
+++ src/main/java/com/berke/urlshortener/dto/CreateShortUrlRequest.java
@@ -0,0 +1,14 @@
+package com.berke.urlshortener.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateShortUrlRequest {
+    private String originalUrl;
+}
---
+++ src/main/java/com/berke/urlshortener/dto/ShortUrlResponse.java
@@ -0,0 +1,18 @@
+package com.berke.urlshortener.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortUrlResponse {
+    
+    private String shortCode;
+    private String originalUrl;
+    private LocalDateTime createdDate;
+}
